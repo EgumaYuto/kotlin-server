75b86770b7687759d29d9161fe2d10c030b56df7
cabos <yuto.eguma@gmail.com>
2018-05-26 00:00:26 +0900

    get extention

--- src/main/kotlin/io/github/egumayuto/contents/Contents.kt
+++ src/main/kotlin/io/github/egumayuto/contents/Contents.kt
@@ -3,4 +3,11 @@ package io.github.egumayuto.contents
 /**
  * @author cabos
  */
-class Contents(val filePath: String, val data: ByteArray = ByteArray(0))
+class Contents(val filePath: String, val data: ByteArray = ByteArray(0)) {
+
+    val extension: String
+        get() {
+            val stringsByPeriod = this.filePath.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            return stringsByPeriod[stringsByPeriod.lastIndex]
+        }
+}
